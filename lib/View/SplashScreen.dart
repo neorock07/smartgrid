852995cb8d538a3f02969cd7ed6b3a27689dec0f
Eka Yulianto <thepilotrock07@gmail.com>
2024-08-10 18:52:06 +0700

    delete : unused package

--- lib/View/SplashScreen.dart
+++ lib/View/SplashScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({ Key? key }) : super(key: key);
@@ -11,7 +12,14 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(""),
+      color: Color.fromRGBO(2, 138, 234, 1),
+      child: Text("SmartGrid",
+      style: TextStyle(
+        fontFamily: "Lato",
+        fontSize: 20.sp,
+        fontWeight: FontWeight.bold,
+        color: Colors.white
+      ),),
     );
   }
 }
